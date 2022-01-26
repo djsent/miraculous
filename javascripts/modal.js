a548729e566c00b478258c384a3afeb66884928a
xman2156 <xman2156@gmail.com>
2022-01-25 19:57:51 -0600

    Fixed modal bug causing background to dissapear

--- javascripts/modal.js
+++ javascripts/modal.js
@@ -6,7 +6,7 @@ var body = document.body.style;
 var i;
 
 function dim() {
-  body.background = "linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('background.webp')";
+  body.background = "linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('images/background.webp')";
   body.backgroundColor = "#4d4c48";
   body.backgroundSize = "cover";
   body.backgroundRepeat = "no-repeat";
@@ -20,7 +20,7 @@ function dim() {
 }
 
 function bright() {
-  body.background = "url('background.webp')";
+  body.background = "url('images/background.webp')";
   body.backgroundColor = "#4d4c48";
   body.backgroundSize = "cover";
   body.backgroundRepeat = "no-repeat";
